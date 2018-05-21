e20892ebe3594b543eddb3b6618d053c2da9e19d
trulycolorful <39491418+trulycolorful@users.noreply.github.com>
2018-05-21 10:59:08 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,8 +2,6 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('message', message => {
-  message.reply(message.author);
-  message.reply('The rules consist of\n\t✦16+ years of age.');
   if (message.author === 'Victoria#8844') {
     message.reply('The rules consist of\n\t✦16+ years of age.');
   }
