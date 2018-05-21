5967fc2a6957bcd5e48a32ac1a44c0b8e39adefb
trulycolorful <39491418+trulycolorful@users.noreply.github.com>
2018-05-21 11:27:19 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,9 +1,14 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+var messageCount = 0;
+
 client.on('message', message => {
-  if (message.author.tag === 'Allanach7#9070' || message.author.tag === 'SquidgyToes#2560') {
-    message.reply('The rules consist of\n\t✦16+ years of age.');
+  if (message.author.tag === 'Victoria#8844'|| message.author.tag === 'Allanach7#9070' || message.author.tag === 'SquidgyToes#2560') {
+    if(messageCount % 20 === 0) {
+      message.reply('The rules consist of\n\t✦16+ years of age.');
+    }
+    messageCount++;
   }
 });
 
