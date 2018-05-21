a447060ff567d400db51ae3f243448798b055c9d
trulycolorful <39491418+trulycolorful@users.noreply.github.com>
2018-05-21 11:30:49 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ const client = new Discord.Client();
 var messageCount = 0;
 
 client.on('message', message => {
-  if (message.author.tag === 'Victoria#8844'|| message.author.tag === 'Allanach7#9070' || message.author.tag === 'SquidgyToes#2560') {
+  if (message.author.tag === 'Allanach7#9070' || message.author.tag === 'SquidgyToes#2560') {
     if(messageCount % 20 === 0) {
       message.reply('The rules consist of\n\t✦16+ years of age.');
     }
